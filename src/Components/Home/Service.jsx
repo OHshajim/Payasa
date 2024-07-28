2b44fc228c88694154a0b9ae1329bf16cd106f06
OHshajim <ajshajimmax@gmail.com>
2024-07-29 04:42:24 +0600

    services update

--- src/Components/Home/Service.jsx
+++ src/Components/Home/Service.jsx
@@ -1,24 +1,39 @@
 import { Link } from "react-router-dom";
 
 const Service = () => {
+  const services = [
+    {
+      service: "Send Money",
+      icon: "/sendmoney.png",
+      route: "/sendmoney",
+    },
+    {
+      service: "Cash Out",
+      icon: "/cashout.png",
+      route: "/sendmoney",
+    },
+    {
+      service: "Add Money",
+      icon: "/addmoney.png",
+      route: "/sendmoney",
+    },
+  ];
   return (
     <div className=" bg-black my-5 rounded-t-[70px] px-10  ">
       <h3 className="pt-10 text-xl font-semibold">Services</h3>
       <div className="mt-10 flex gap-10">
-        <Link to={"/sendmoney"}>
-          <button>
-            <img src="/sendmoney.png" alt="" className="w-12 mx-auto pb-2" />
-            Send Money
-          </button>
-        </Link>
-        <button>
-          <img src="/cashout.png" alt="" className="w-12 mx-auto pb-2" />
-          Cash Out
-        </button>
-        <button>
-          <img src="/addmoney.png" alt="" className="w-10 mx-auto pb-2" />
-          Add Money
-        </button>
+        {services.map((service) => (
+          <Link to={service.route} key={service.service}>
+            <button>
+              <img
+                src={service.icon}
+                alt={service.service}
+                className="w-12 mx-auto pb-2"
+              />
+              {service.service}
+            </button>
+          </Link>
+        ))}
       </div>
     </div>
   );
--- src/Shared/ServiceNav.jsx
+++ src/Shared/ServiceNav.jsx
@@ -3,15 +3,18 @@ import { BiArrowBack } from "react-icons/bi";
 const ServiceNav = ({ service }) => {
   return (
     <div className="navbar  bg-green-950 select-none">
-      <button className="btn btn-ghost text-2xl rounded-full ">
-        <BiArrowBack />
-      </button>
+      <Link to={"/"}>
+        <button className="btn btn-ghost text-2xl rounded-full ">
+          <BiArrowBack />
+        </button>
+      </Link>
       <h4 className="font-semibold text-lg">{service}</h4>
     </div>
   );
 };
 
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 ServiceNav.propTypes = {
   service: PropTypes.string.isRequired,
 };
