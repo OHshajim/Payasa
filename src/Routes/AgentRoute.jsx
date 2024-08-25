29737aadd2b21ccae2e3bcb45174460498407f53
OHshajim <ajshajimmax@gmail.com>
2024-08-25 23:21:11 +0600

    agentRoute add

--- src/Routes/AgentRoute.jsx
+++ src/Routes/AgentRoute.jsx
@@ -1,21 +1,21 @@
-import { Navigate } from "react-router-dom";
 import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
 import PropTypes from "prop-types";
 
-const AdminRoute = ({ children }) => {
+const AgentRoute = ({ children }) => {
   const AccessKey = localStorage.getItem("access_key");
   const { user } = useContext(AuthContext);
   if (!AccessKey) {
     return <Navigate to={"/login"} />;
   }
-  if (user?.status === "Admin") {
+  if (user?.status === "Agent") {
     return children;
   }
   return <Navigate to={"/login"} />;
 };
 
-AdminRoute.propTypes = {
+AgentRoute.propTypes = {
   children: PropTypes.element.isRequired,
 };
-export default AdminRoute;
+export default AgentRoute;
