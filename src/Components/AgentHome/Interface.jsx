e247c78488f2d661683001341e0b2fab51f64ddc
OHshajim <ajshajimmax@gmail.com>
2024-09-03 21:37:45 +0600

    agent site  update

--- src/Components/AgentHome/Interface.jsx
+++
@@ -1,9 +0,0 @@
-const Interface = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default Interface;
--- src/Pages/Agent/AgentDashBoard.jsx
+++ src/Pages/Agent/AgentDashBoard.jsx
@@ -1,9 +1,20 @@
-import Interface from "../../Components/AgentHome/Interface";
+import Balance from "../../Components/Home/Balance";
+import Footer from "../../Components/Home/Footer";
+import Nav from "../../Components/Home/Nav";
+// import Service from "../../Components/Home/Service";
 
 const AgentDashBoard = () => {
   return (
-    <div>
-      <Interface />
+    <div className="relative select-none">
+      <div className="z-50">
+        <div className="mx-14">
+          <Nav />
+          <Balance />
+        </div>
+        {/* <Service /> */}
+        <Footer />
+      </div>
+      <div className="absolute bg-green-900 w-screen h-[35vh] top-0 -z-50 banner"></div>
     </div>
   );
 };
